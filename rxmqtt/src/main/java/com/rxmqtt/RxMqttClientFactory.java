22353f40245d37582d40dbbeb1a89b37cecb6876
Nihad Ahmetovic <nihad92@gmail.com>
2018-07-17 11:07:00 +0200

    Added additional methodes for creating mqtt client

--- rxmqtt/src/main/java/com/rxmqtt/RxMqttClientFactory.java
+++ rxmqtt/src/main/java/com/rxmqtt/RxMqttClientFactory.java
@@ -5,14 +5,25 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class RxMqttClientFactory {
 
+  public RxMqttClient create(String url, String clientId) throws
+      MqttException {
+    return new RxMqttClientImpl(url, clientId);
+  }
+
   public RxMqttClient create(String host, Integer port, String clientId, Boolean useSSL) throws
       MqttException {
     return new RxMqttClientImpl(getBrokerUrl(host, port, useSSL), clientId);
   }
 
+
   public RxMqttClient create(String host, Integer port, String clientId, Boolean useSSL, String username, String password)
       throws MqttException {
     return new RxMqttClientImpl(getBrokerUrl(host, port, useSSL), clientId, username, password);
+  }
+
+  public RxMqttClient create(String url, String clientId, String username, String password)
+      throws MqttException {
+    return new RxMqttClientImpl(url, clientId, username, password);
   }
 
   private String getBrokerUrl(String host, int port, Boolean useSSL) {
